036fc90aebaebd2c267e974d594ae9e8633d4b09
Thomas Wunderlich <twunderlich@napower.com>
2016-01-24 21:03:21 -0500

    Consolo books demo app created

---
+++ src/ConsoloBooks/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoloBooks.Models
+{
+    public class Author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ src/ConsoloBooks/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoloBooks.Models
+{
+    public class Book
+    {
+        [ScaffoldColumn(false)]
+        public int BookID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        [Range(1, 500)]
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        //navigation property
+        public virtual Author Author { get; set; }
+
+    }
+}
